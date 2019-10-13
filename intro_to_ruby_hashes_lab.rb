31a8e0c14fb0ab6faf1324c8f9c0385fa3c36f0c
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-13 20:37:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
- read_from_hash = {"key" => "value"} 
+ read_from_hash = {key => "value"} 
  #return read_from_hash[key]  
  
  
