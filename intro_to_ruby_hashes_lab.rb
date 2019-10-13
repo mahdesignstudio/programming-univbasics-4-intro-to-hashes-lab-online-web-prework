4027b6d621f4a509c18319e433836fb4349e02d5
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-13 23:03:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash = Hash.new 
-  read_from_hash ={
-    name: "Sam"
-  }
-  person[:name]
+  hash = {key} 
 end 
 
 def update_counting_hash(hash, key)
