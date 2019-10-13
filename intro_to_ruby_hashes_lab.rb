e38f97fe73121bba241c4798bf42edd356a74b31
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-13 23:25:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,11 +31,8 @@ def read_from_hash(hash, key)
 end 
 
 def update_counting_hash(hash, key)
-  update_counting_hash = {
-    name:"Sam"
-    age:31
-  }
-  return update_counting_hash[:age]
+   update_counting_hash = {hash => key}
+  return update_counting_hash
   
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
