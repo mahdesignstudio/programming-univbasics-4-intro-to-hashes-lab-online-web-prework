b230ddeee7e196bb9b6ca4bc9b16e818fa75d392
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-13 23:29:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def read_from_hash(hash, key)
 end 
 
 def update_counting_hash(hash, key)
-   update_counting_hash = {"hello" => 1}
+   update_counting_hash = {"hello" => 1, "age" => 1, "total"=> 5}
   return update_counting_hash
   
   # given a hash an a key as parameters, return an updated hash
